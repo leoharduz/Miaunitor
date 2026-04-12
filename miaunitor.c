69e42fc3e853787c74dcbd8d6038c861de14b1d1
Leonardo Goncalves <leonardogoncalves@mail.com>
2026-04-11 23:08:11 -0300

    feat: Inicializa pinos de saida para LEDs, e Buzzers com PWM

--- miaunitor.c
+++ miaunitor.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/adc.h" // Biblíoteca para usar o adc, sensores e joystick
+#include "hardware/adc.h" // Biblioteca para usar o ADC, sensores e joystick
+#include "hardware/pwm.h" // Biblioteca para usar o PWM
 
 #define CANAL_JOYSTICK_Y 1
 #define PINO_JOYSTICK_Y 27
 #define CANAL_SENSOR_INTERNO 4
 #define AJUSTE_TEMPERATURA 5.0f // Maximo de variacao de temperatura pelo joystick eixo y
+#define LED_VERMELHO 13
+#define LED_AZUL 12
+#define BUZZER_A 21
+#define BUZZER_B 10
+#define TEMP_ALTA 40.0f // Temperatura máxima sem acionar alarme
+#define TEMP_BAIXA 29.99f // Temperatura mínima sem acionar alarme
+
+// Funcao para inicializar as saidas
+void iniciar_saidas(){
+    // Inicializar os LEDS
+    gpio_init(LED_VERMELHO);
+    gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+    gpio_init(LED_AZUL);
+    gpio_set_dir(LED_AZUL, GPIO_OUT);
+
+    // Inicializar os BUZZERS
+    gpio_set_function(BUZZER_A, GPIO_FUNC_PWM);
+    gpio_set_function(BUZZER_B, GPIO_FUNC_PWM);
+    // Configura BUZZER A
+    uint slice_a = pwm_gpio_to_slice_num(BUZZER_A);
+    pwm_set_wrap(slice_a, 4000); // Define a frequencia
+    pwm_set_chan_level(slice_a, PWM_CHAN_B, 0); // Comeca desligado (volume 0)
+    pwm_set_enabled(slice_a, true);
+    // Configura BUZZER B
+    uint slice_b = pwm_gpio_to_slice_num(BUZZER_B);
+    pwm_set_wrap(slice_b, 4000); // Define a frequencia
+    pwm_set_chan_level(slice_b, PWM_CHAN_A, 0); // Comeca desligado (volume 0)
+    pwm_set_enabled(slice_b, true);
+}
 
 int main() {
     stdio_init_all();
